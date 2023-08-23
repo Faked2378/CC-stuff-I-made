f852b9d4db750a789191faa888313950f60a38e7
Fakedrift <127430703+Faked2378@users.noreply.github.com>
2023-08-23 12:14:45 +0100

    .

--- MineOS/installer.lua
+++ MineOS/installer.lua
@@ -1,3 +1,11 @@
-local up = 
-
-if key
+while true do
+    local event, key = os.pullEvent("key")
+    
+    -- 'event' will be "key" and 'key' will hold the key code of the pressed key
+    print("Key pressed: " .. key)
+    
+    if key == keys.q then
+        print("Exiting program.")
+        break
+    end
+end
