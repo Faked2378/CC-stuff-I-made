379ae81e7e3f37e8e37528986fd0b7cae97a96aa
Fakedrift <127430703+Faked2378@users.noreply.github.com>
2023-08-23 13:18:30 +0100

    Update installer.lua

--- MineOS/installer.lua
+++ MineOS/installer.lua
@@ -19,32 +19,30 @@ while true do
             sel = "graph"
         end
     end
-    if sel == "graph" then
+    if sel == "term" then
         term.setCursorPos(1,2)
         print("    graphical")
         term.setCursorPos(1,3)
         print("--> terminal")
         term.setCursorPos(0,0)
+	if key == keys.enter then
+		term.setCursorPos(1,5)
+		print("terminal selected")
+	end
     end
-    if sel == "term" then
+    if sel == "graph" then
         term.setCursorPos(1,2)
         print("--> graphical")
         term.setCursorPos(1,3)
         print("    terminal")
         term.setCursorPos(0,0)
-	end
 	if key == keys.enter then
-		if sel == "graph" then
-			term.setCursorPos(1,5)
-			print("graphical selected")
-			os.sleep(2)
-			term.clear()
-			local graphDIR = "https://raw.githubusercontent.com/Faked2378/CC-stuff-I-made/main/MineOS/1-0/graphic/installerG.lua"
-			shell.run("wget", "run", graphDIR)
-		end
-		if sel == "term" then
-			term.setCursorPos(1,5)
-			print("terminal selected")
-		end
+		term.setCursorPos(1,5)
+		print("graphical selected")
+		os.sleep(2)
+		term.clear()
+		local graphDIR = "https://raw.githubusercontent.com/Faked2378/CC-stuff-I-made/main/MineOS/1-0/graphic/installerG.lua"
+		shell.run("wget", "run", graphDIR)
 	end
+    end
 end
