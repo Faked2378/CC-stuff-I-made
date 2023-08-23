90faae0f9c27bd3baa7a34f8b3c1b2f79e1d48ac
Fakedrift <127430703+Faked2378@users.noreply.github.com>
2023-08-23 12:54:59 +0100

    Update installerG.lua

--- MineOS/1-0/graphic/installerG.lua
+++ MineOS/1-0/graphic/installerG.lua
@@ -1,4 +1,29 @@
-term.clear()
+local menuCode = "EMPxvvEZ"
+local settingsCode = "uReswmYn"
+local uninDIR = "https://raw.githubusercontent.com/Faked2378/CC-stuff-I-made/main/MineOS/1-0/graphic/uninst.lua"
 
-local menuCode = EMPxvvEZ
-shell.run("pastebin", "get", menucode, "menu")
+-- Download and execute the menu script
+if shell.run("pastebin", "get", menuCode, "mineOS/menu") then
+  print("Menu downloaded successfully (25% done)")
+  
+  -- Download and execute the settings script
+  if shell.run("pastebin", "get", settingsCode, "mineOS/settings") then
+    print("Settings downloaded successfully (40% done)")
+    
+    -- Download and execute the uninstall script
+    if shell.run("wget", uninDIR, "mineOS/uninst") then
+      print("Uninstall script downloaded successfully (90% done)")
+      
+      print("Performing final touches")
+      os.sleep(7)
+      term.clear()
+      print("Installation done")
+    else
+      print("Failed to download uninstall script")
+    end
+  else
+    print("Failed to download settings script")
+  end
+else
+  print("Failed to download menu script")
+end
