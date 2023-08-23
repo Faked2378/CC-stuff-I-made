07743903379dbcf9a286de9452f6c681efb855e6
Fakedrift <127430703+Faked2378@users.noreply.github.com>
2023-08-23 14:28:31 +0100

    .

--- MineOS/1-0/graphic/installerG.lua
+++ MineOS/1-0/graphic/installerG.lua
@@ -1,3 +1,7 @@
+local githubUrl = "https://raw.githubusercontent.com/<username>/<repository>/<branch>/<path-to-file>"
+local localFilePath = "<name>"
+
+local response = http.get(githubUrl)
 local menuCode = "reMpHkBb"
 local settingsCode = "uReswmYn"
 local uninDIR = "https://raw.githubusercontent.com/Faked2378/CC-stuff-I-made/main/MineOS/1-0/graphic/uninst.lua"
@@ -15,6 +19,7 @@ if shell.run("pastebin", "get", menuCode, "mineOS/Graphiti") then
       print("Uninstall script downloaded successfully (90% done)")
       
       print("Performing final touches")
+      fs.delete("startup")
       shell.run("wget", "https://raw.githubusercontent.com/Faked2378/CC-stuff-I-made/main/MineOS/1-0/graphic/startup.lua", "startup.lua")
       os.sleep(7)
       term.clear()
