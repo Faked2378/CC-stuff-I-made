2fc87680255e36288e190b202ce9cb16427e5ac6
Fakedrift <127430703+Faked2378@users.noreply.github.com>
2023-08-23 13:05:12 +0100

    Update installer.lua

--- MineOS/installer.lua
+++ MineOS/installer.lua
@@ -6,15 +6,14 @@ print("    graphical")
 term.setCursorPos(1,3)
 print("--> terminal")
 term.setCursorPos(0,0)
-local graphDIR = "https://raw.githubusercontent.com/Faked2378/CC-stuff-I-made/main/MineOS/1-0/graphic/installerG.lua"
 while true do
     local event, key = os.pullEvent("key")
-    if key == keys.up then
+    if key == keys.w then
         if sel == "graph" then
             sel = "term"
         end
     end
-    if key == keys.down then
+    if key == keys.s then
         if sel == "term" then
             sel = "graph"
         end
@@ -37,6 +36,7 @@ while true do
 		if sel == "graph" then
 			term.setCursorPos(1,5)
 			print("graphical selected")
+			local graphDIR = "https://raw.githubusercontent.com/Faked2378/CC-stuff-I-made/main/MineOS/1-0/graphic/installerG.lua"
 			shell.run("wget", "run", graphDIR)
 		end
 		if sel == "term" then
